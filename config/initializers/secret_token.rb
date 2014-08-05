e95bb915234a1011865a2c3407f34a812ee79248
Ronan Clarke <rclarke@whatclinic.com>
2014-08-05 20:40:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NinefoldTest::Application.config.secret_key_base = 'df827915a8631630903cabfbb348b1c2a5fb2276d133132d94c9f50621e9104f277c1fdc8f81c6988a1b7c4a1d73cd81399b5ca6ad7804d7dd0c62588698a379'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NinefoldTest::Application.config.session_store :cookie_store, key: '_ninefold-test_session'
